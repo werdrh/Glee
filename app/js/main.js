797268e381449d93e4d5258055f5feaea681d174
werdrh <werdrh@gmail.com>
2020-12-01 13:14:53 +0200

    added design section

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,19 @@
-$(function (){
-    $('.top-slider__inner').slick({
-       dots:true,
-       arrows:false,
-       autoplay:true
-    
-      });
-      var mixer = mixitup('.products__items', {
-        
-    });
+$(function () {
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true
+
+  });
+
+  var mixer1 = mixitup('.products__items', {
+    selectors: {
+      control: '.products__nav-btn'
+    }
+  });
+  var mix2 = mixitup('.design__inner', {
+    selectors: {
+      control: '.design__nav-btn'
+    }
+  });
 });
