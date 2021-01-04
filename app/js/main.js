998505d60de884b131966bb666e51ee60aaa7de6
werdrh <werdrh@gmail.com>
2021-01-04 13:46:28 +0200

    Added product page content

--- app/js/main.js
+++ app/js/main.js
@@ -11,9 +11,9 @@ $(function () {
   $('.product-detalis__top-slider-small').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    vertical:true,
+    vertical: true,
     centerPadding: '0px',
-    
+
     asNavFor: '.product-detalis__top-slider-big',
     dots: false,
     centerMode: false,
@@ -22,12 +22,12 @@ $(function () {
 
   $(".star").rateYo({
     readOnly: true,
-    starSvg:'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18pt" height="16pt" viewBox="0 0 18 16" version="1.1"><g id="surface1"><path style=" stroke:none;fill-rule:nonzero;fill-opacity:1;" d="M 8.101562 0.554688 L 6.0625 4.695312 L 1.496094 5.359375 C 0.679688 5.476562 0.351562 6.488281 0.945312 7.066406 L 4.246094 10.285156 L 3.464844 14.832031 C 3.324219 15.652344 4.191406 16.269531 4.914062 15.882812 L 9 13.738281 L 13.085938 15.882812 C 13.808594 16.265625 14.675781 15.652344 14.535156 14.832031 L 13.753906 10.285156 L 17.054688 7.066406 C 17.648438 6.488281 17.320312 5.476562 16.503906 5.359375 L 11.9375 4.695312 L 9.898438 0.554688 C 9.53125 -0.179688 8.472656 -0.191406 8.101562 0.554688 Z M 8.101562 0.554688 "/></g></svg>'   
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18pt" height="16pt" viewBox="0 0 18 16" version="1.1"><g id="surface1"><path style=" stroke:none;fill-rule:nonzero;fill-opacity:1;" d="M 8.101562 0.554688 L 6.0625 4.695312 L 1.496094 5.359375 C 0.679688 5.476562 0.351562 6.488281 0.945312 7.066406 L 4.246094 10.285156 L 3.464844 14.832031 C 3.324219 15.652344 4.191406 16.269531 4.914062 15.882812 L 9 13.738281 L 13.085938 15.882812 C 13.808594 16.265625 14.675781 15.652344 14.535156 14.832031 L 13.753906 10.285156 L 17.054688 7.066406 C 17.648438 6.488281 17.320312 5.476562 16.503906 5.359375 L 11.9375 4.695312 L 9.898438 0.554688 C 9.53125 -0.179688 8.472656 -0.191406 8.101562 0.554688 Z M 8.101562 0.554688 "/></g></svg>'
   });
 
   $(".filter-price__input").ionRangeSlider({
     step: 0.01,
-    onStart: function (data){      
+    onStart: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
     },
@@ -35,9 +35,9 @@ $(function () {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
     }
-    
+
   });
-  
+
 
   $('.top-slider__inner').slick({
     dots: true,
@@ -46,20 +46,26 @@ $(function () {
 
   });
 
-  
- var mix2 = mixitup('.design__inner', {
+  $('.product-detalis__bottom-content').slick({
+    arrows: true,
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+
+  var mix2 = mixitup('.design__inner', {
     selectors: {
       control: '.design__nav-btn'
     }
   });
-  
+
   var mixer1 = mixitup('.products__items', {
     selectors: {
       control: '.products__nav-btn'
     }
   });
 
- 
+
 
 });
 
@@ -69,6 +75,6 @@ var mixer3 = mixitup('.product-detalis__info-items', {
   },
   load: {
     filter: '.category-1'
-}
- 
+  }
+
 });
